66cc9916f9d9ac0c89deaae3762b6613247ce356
rafatellescarneiro <rafatellescarneiro@gmail.com>
2022-06-25 08:44:29 -0300

    devinhouse/alteracoes
    algumas alterações/ criação da novaUnidade para cadastro de unidades

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -7,7 +7,7 @@
           <div class="row g-0">
             <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
               <div class="text-white px-3 py-4 p-md-5 mx-md-4">
-                <img src="../assets/logo.png" alt="" srcset="">
+                <img src="../assets/logo.png" alt="placa solar">
               </div>
             </div>
             <div class="col-lg-6">
